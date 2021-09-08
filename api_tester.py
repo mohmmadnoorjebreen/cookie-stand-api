23e969cd327d1c822b9dfb90019b92fb72b18e37
MOH <mohammadnoormjk1998@gmail.com>
2021-09-08 23:52:28 +0300

    update

--- api_tester.py
+++ api_tester.py
@@ -1,10 +1,10 @@
 import fire
 import requests
 
-API_HOST = "http://localhost:8000"
-RESOURCE_URI = "things"
-USERNAME = "admin"
-PASSWORD = "admin"
+API_HOST = "http://http://sport-1.herokuapp.com"
+RESOURCE_URI = "sport"
+USERNAME = "mohammad"
+PASSWORD = "mohammad"
 
 
 class ApiTester:
